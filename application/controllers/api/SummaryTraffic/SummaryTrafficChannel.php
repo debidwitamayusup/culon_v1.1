dc831d3b52f02050ead2edd58410c683eaea3a84
Risyad Dimas <risyad.dimas@gmail.com>
2019-10-30 14:00:03 +0700

    API Summary Traffic Channel D/M/Y

---
+++ application/controllers/api/SummaryTraffic/SummaryTrafficChannel.php
@@ -0,0 +1,158 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SummaryTrafficChannel extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Stc_Model');
+	}
+	// public function index()
+	// {
+	// 	$this->load->view('header');
+	// 	$this->load->view('navbar');
+	// 	$this->load->view('sidebar');
+	// 	$this->load->view('stc/Summary_Call');
+	// 	$this->load->view('footer');
+	// }	
+
+	// public function tc_menu()
+	// {
+	// 	$this->load->view('header');
+	// 	$this->load->view('navbar');
+	// 	$this->load->view('sidebar');
+	// 	$this->load->view('stc/Traffic_Channel');
+	// 	$this->load->view('footer');
+	// }
+
+	// public function avr_time_menu()
+	// {
+	// 	$this->load->view('header');
+	// 	$this->load->view('navbar');
+	// 	$this->load->view('sidebar');
+	// 	$this->load->view('stc/Average_Time');
+	// 	$this->load->view('footer');
+	// }
+
+	// public function case_menu()
+	// {
+	// 	$this->load->view('header');
+	// 	$this->load->view('navbar');
+	// 	$this->load->view('sidebar');
+	// 	$this->load->view('stc/Case_In_Out');
+	// 	$this->load->view('footer');
+	// }
+
+	//tampil perhari
+	public function stc_today()
+	{
+		//proses select data
+		$rowdate = $this->Stc_Model->getToday();
+
+		//response true false
+		if($rowdate)
+		{
+			$response = array(
+				'status' => true,
+				'data' => $rowdate);
+		} else {
+			$response = array(
+				'status' => false,
+				'data' => 'Data Not Found');
+		}
+		
+
+		echo json_encode($response);
+	}
+
+	//tampil perbulan
+	public function stc_month()
+	{
+		//proses select data
+		$rowdate = $this->Stc_Model->getMonth();
+
+		//response true false
+		if($rowdate)
+		{
+			$response = array(
+				'status' => true,
+				'data' => $rowdate);
+		} else {
+			$response = array(
+				'status' => false,
+				'data' => 'Data Not Found');
+		}
+		
+
+		echo json_encode($response);
+	}
+
+	//tampil pertahun
+	public function stc_year()
+	{
+		//proses select data
+		$rowdate = $this->Stc_Model->getYear();
+
+		//response true false
+		if($rowdate)
+		{
+			$response = array(
+				'status' => true,
+				'data' => $rowdate);
+		} else {
+			$response = array(
+				'status' => false,
+				'data' => 'Data Not Found');
+		}
+		
+
+		echo json_encode($response);
+	}
+
+	// public function stc_interval()
+	// {
+
+	// }
+
+	// public function stc_art()
+	// {
+
+	// }
+
+	// public function stc_aht()
+	// {
+
+	// }
+
+	// public function stc_ast()
+	// {
+
+	// }
+
+	// public function case_in_interval()
+	// {
+
+	// }
+
+	// public function case_out_interval()
+	// {
+
+	// }
+}
--- application/models/Stc_Model.php
+++ application/models/Stc_Model.php
@@ -2,8 +2,39 @@
 
 class Stc_Model extends CI_Model
 {
-	public function getToday($date)
+
+	public function __construct()
 	{
-		return $this->d
+		parent:: __construct();
+	}
+
+	// select data table hsummary
+	//controller SummaryTrafficChannel function stc_today
+	public function getToday()
+	{
+		// $date = date('lup') = 'CURRENT_DATE';
+		// $this->db->select('channel,
+		// SUM(tot_pickup) + SUM(antrian) + SUM(chat_in_progress) AS summary_traffic');
+		// $this->db->from('hsummary_copy');
+		// $this->db->where($date);
+		// $this->db->group_by('channel');
+		$query = $this->db->query('SELECT channel, SUM(tot_pickup) + SUM(antrian) + SUM(chat_in_progress) AS summary_traffic FROM hsummary_copy WHERE date(lup) = CURRENT_DATE GROUP BY channel;');
+		return $query->result();
+	}
+
+	//controller SummaryTrafficChannel function stc_month
+	public function getMonth()
+	{
+		$query = $this->db->query('SELECT channel, SUM(tot_pickup) + SUM(antrian) + SUM(chat_in_progress) AS summary_traffic
+				FROM hsummary_copy WHERE MONTH(lup) = MONTH(CURRENT_TIME) AND YEAR(lup) = YEAR(CURRENT_TIME) group by channel;');
+		return $query->result();
+	}
+
+	//controller SummaryTrafficChannel function stc_year
+	public function getYear()
+	{
+		$query = $this->db->query('SELECT channel, sum(tot_pickup) + sum(antrian) + sum(chat_in_progress) AS summary_traffic
+				FROM hsummary_copy WHERE YEAR(lup) = YEAR(CURRENT_TIME) group by channel;');
+		return $query->result();
 	}
 }
