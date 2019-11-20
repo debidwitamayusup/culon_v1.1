dfd5910ef5be93969871b74be38a5f1207f8bb55
trybio <trybio123@gmail.com>
2019-11-20 09:02:36 +0700

    initial commit

---
+++ application/views/header.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+	<head>
+		<!-- Meta data -->
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta content="Spaner - Simple light Bootstrap Nice Admin Panel Dashboard Design Responsive HTML5 Template" name="description">
+		<meta content="Spruko Technologies Private Limited" name="author">
+		<meta name="keywords" content="bootstrap panel, bootstrap admin template, dashboard template, bootstrap dashboard, dashboard design, best dashboard, html css admin template, html admin template, admin panel template, admin dashbaord template, bootstrap dashbaord template, it dashbaord, hr dashbaord, marketing dashbaord, sales dashbaord, dashboard ui, admin portal, bootstrap 4 admin template, bootstrap 4 admin"/>
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" type="image/x-icon" href="<?= base_url()?>assets/images/brand/favicon.ico" />
+
+		<!-- Title -->
+		<title>Spaner - Simple light Bootstrap Nice Admin Panel Dashboard Design Responsive HTML5 Template</title>
+
+		<!--Bootstrap.min css-->
+		<link rel="stylesheet" href="<?= base_url()?>assets/plugins/bootstrap/css/bootstrap.min.css">
+
+        <!--Font Awesome-->
+		<link href="<?= base_url()?>assets/plugins/fontawesome-free/css/all.css" rel="stylesheet">
+
+		<!-- Dashboard Css -->
+		<link href="<?= base_url()?>assets/css/style.css" rel="stylesheet" />
+		<link href="<?= base_url()?>assets/css/color-styles.css" rel="stylesheet" />
+		<link href="<?= base_url()?>assets/css/skin-modes.css" rel="stylesheet" />
+
+		<!-- Vector-map -->
+		<link href="<?= base_url()?>assets/plugins/jquery.vmap/jqvmap.min.css" rel="stylesheet">
+
+		<!-- Custom scroll bar css-->
+		<link href="<?= base_url()?>assets/plugins/jquery.mCustomScrollbar/jquery.mCustomScrollbar.css" rel="stylesheet" />
+
+		<!--Sidemenu css-->
+		<link rel="stylesheet" href="<?= base_url()?>assets/plugins/side-menu/side-menu.css">
+
+		<!--Sidemenu-responsive-tabs  css -->
+		<link href="<?= base_url()?>assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css" rel="stylesheet">
+
+		<!-- P-scroll css -->
+		<link href="<?= base_url()?>assets/plugins/p-scroll/p-scroll.css" rel="stylesheet" type="text/css">
+
+		<!-- morris Charts Plugin -->
+		<link href="<?= base_url()?>assets/plugins/morris/morris.css" rel="stylesheet" />
+
+		<!---Font icons-->
+		<link href="<?= base_url()?>assets/plugins/iconfonts/plugin.css" rel="stylesheet" />
+
+		<!-- Sidebar css -->
+		<link href="<?= base_url()?>assets/plugins/sidebar/sidebar.css" rel="stylesheet">
+
+	</head>
+</html>
