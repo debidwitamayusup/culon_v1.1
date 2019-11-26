13ec3da04d8d22cbe5870353c08521b6a83b8895
Elsa Maitsa <elsamaitsa@gmail.com>
2019-11-26 13:42:00 +0700

    El

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,6 +1,4 @@
-<!-- Sidebar menu-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
-				<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 				<aside class="app-sidebar">
 					<div class="side-tab-body p-0 border-0" id="sidemenu-Tab">
 						<div class="first-sidemenu">
@@ -410,59 +408,3 @@
 					</div>
 				</aside>
 				<!--sidemenu end-->
-
-				<aside class="app-sidebar">
-					<div class="app-sidebar__user">
-						<div class="user-body">
-							<span class="avatar avatar-lg brround text-center cover-image" data-image-src="../assets/images/users/5.jpg"></span>
-						</div>
-						<div class="user-info">
-							<a href="#" class="ml-2"><span class="text-dark app-sidebar__user-name font-weight-semibold">On 4 </span><br>
-								<span class="text-muted app-sidebar__user-name text-sm">Dashboard</span>
-							</a>
-						</div>
-					</div>
-					<ul class="side-menu">
-						<li>
-							<a class="side-menu__item" href="<?= base_url()?>home"><i class="side-menu__icon si si-home"></i><span class="side-menu__label">Home</span></a>
-						</li>
-						<li class="slide">
-							<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon si si-chart"></i><span class="side-menu__label">Traffic</span><i class="angle fas fa-angle-right"></i></a>
-							<ul class="slide-menu">
-								<li><a class="slide-item" href="index.html">Summary Traffic</a></li>
-								<li><a class="slide-item" href="index2.html">Traffic Interval</a></li>
-								<li><a class="slide-item" href="index3.html">Average Time</a></li>
-								<li><a class="slide-item" href="index4.html">Case In / Out</a></li>
-							</ul>
-						</li>
-						<li class="slide">
-							<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon si si-user"></i><span class="side-menu__label">Agent Performance</span><i class="angle fas fa-angle-right"></i></a>
-							<ul class="slide-menu">
-								<li><a class="slide-item" href="<?= base_url();?>Summary_Call_Agent">Summary Call</a></li>
-								<li><a class="slide-item" href="index2.html">Interval Call</a></li>
-								<li><a class="slide-item" href="index3.html">Average Time</a></li>
-								<li><a class="slide-item" href="index4.html">Case In / Out</a></li>
-							</ul>
-						</li>
-						<li class="slide">
-							<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon si si-briefcase"></i><span class="side-menu__label">Operation Performance</span><i class="angle fas fa-angle-right"></i></a>
-							<ul class="slide-menu">
-								<li><a class="slide-item" href="index.html">Summary Traffic</a></li>
-								<li><a class="slide-item" href="index2.html">Traffic Interval</a></li>
-								<li><a class="slide-item" href="index3.html">Average Time</a></li>
-								<li><a class="slide-item" href="index4.html">Case In / Out</a></li>
-							</ul>
-						</li>
-						<li class="slide">
-							<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon si si-docs"></i><span class="side-menu__label">Summarty Ticket</span><i class="angle fas fa-angle-right"></i></a>
-							<ul class="slide-menu">
-								<li><a class="slide-item" href="index.html">Summary Traffic</a></li>
-								<li><a class="slide-item" href="index2.html">Traffic Interval</a></li>
-								<li><a class="slide-item" href="index3.html">Average Time</a></li>
-								<li><a class="slide-item" href="index4.html">Case In / Out</a></li>
-							</ul>
-						</li>
-						
-					</ul>
-				</aside>
-				<!--side-menu-->
