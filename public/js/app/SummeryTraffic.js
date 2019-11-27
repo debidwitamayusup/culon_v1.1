3b862770052c97b01f48532e4da1742565d1529e
try haryatno <trybio123@gmail.com>
2019-11-27 11:40:39 +0700

    memperbaiki bentuk data yang akan di distribusi pada frontend

--- public/js/app/SummeryTraffic.js
+++ public/js/app/SummeryTraffic.js
@@ -18,20 +18,6 @@ $(document).ready(function(){
 
 	function drawchart(r){
 		/*----Echart2----*/
-		// console.log(r.data.length);
-		//proses pengelompokan
-		var data_x = [];
-		var data_y = [];
-		var data_msg_in = [];
-		var i = 0;
-		for(i = 0; i < r.data.length; i++){
-			data_x.push(r.data[i].row_date);
-			data_y.push(r.data[i].case_in);
-			data_msg_in.push(r.data[i].msg_in);
-		}
-		// 
-		// console.log(label);
-
 		// draw chart
 		var chart = document.getElementById('echart_tes');
 		var barChart = echarts.init(chart);
@@ -44,7 +30,7 @@ $(document).ready(function(){
 			},
 			xAxis: {
 				// data: ['2014', '2015', '2016', '2017', '2018'],
-				data: data_x,
+				data: r.data.row_date,
 				axisLine: {
 					lineStyle: {
 						color: '#efefff'
@@ -88,12 +74,12 @@ $(document).ready(function(){
 				{
 					name: 'case in',
 					type: 'bar',
-					data: data_y
+					data: r.data.case_in
 				}, 
 				{
 					name: 'msg in',
 					type: 'bar',
-					data: data_msg_in
+					data: r.data.msg_in
 				},
 			],
 			color: ['#089e60', '#ff9933', ]
