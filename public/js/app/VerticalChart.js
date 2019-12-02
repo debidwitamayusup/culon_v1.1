f918a463fe2cf13f040c274f600b281650af08e6
fajarbc <bcfajar@gmail.com>
2019-12-02 14:05:45 +0700

    push baru

--- public/js/app/VerticalChart.js
+++ public/js/app/VerticalChart.js
@@ -2,7 +2,7 @@
 	"use strict";
 	//bar chart
 	var ctx = document.getElementById("echartVertical");
-	ctx.height = 500;
+	ctx.height = 573;
 	var myChart = new Chart(ctx, {
 		type: 'horizontalBar',
 		data: {
@@ -18,49 +18,41 @@
 				"Messenger",
 				"Twitter DM",
 				"Live Chat",
-				"Insta DM"
+				"Pesan"
 			],
 			datasets: [{
-					label: "Data awal",
-					data: [85, 48, 59, 37, 12, 16, 18, 30, 40, 10, 40, 12],
-					borderColor: [
-						"#31a550 ",
-						"#45aaf2",
-						"#316cbe",
-						"#e41313",
-						"#343a40",
-						"#31a550",
-						"#ff9933",
-						"#fbc0d5",
-						"#3866a6",
-						"#6574cd",
-						"#42265e",
-						"#1c3353"
-					],
-					borderWidth: "0",
-					backgroundColor: [
-						"#31a550",
-						"#45aaf2",
-						"#316cbe",
-						"#e41313",
-						"#343a40",
-						"#31a550",
-						"#ff9933",
-						"#fbc0d5",
-						"#3866a6",
-						"#6574cd",
-						"#42265e",
-						"#1c3353"
-					]
-				},
-				{
-					label: "Data akhir",
-					data: [65, 59, 80,65, 59, 80,65, 59, 80,65, 59, 80],
-					borderColor: ["#000", "#000", "#000","#000", "#000", "#000","#000", "#000", "#000","#000", "#000", "#000"],
-					borderWidth: "0",
-					backgroundColor: ["#000", "#000", "#000","#000", "#000", "#000","#000", "#000", "#000","#000", "#000", "#000"]
-				}
-			]
+				label: "Data awal",
+				data: [85, 48, 59, 37, 12, 16, 18, 30, 40, 10, 40, 12],
+				borderColor: [
+					"#31a550 ",
+					"#45aaf2",
+					"#316cbe",
+					"#e41313",
+					"#343a40",
+					"#31a550",
+					"#ff9933",
+					"#fbc0d5",
+					"#3866a6",
+					"#6574cd",
+					"#42265e",
+					"#1c3353"
+				],
+				borderWidth: "0",
+				backgroundColor: [
+					"#31a550",
+					"#45aaf2",
+					"#316cbe",
+					"#e41313",
+					"#343a40",
+					"#31a550",
+					"#ff9933",
+					"#fbc0d5",
+					"#3866a6",
+					"#6574cd",
+					"#42265e",
+					"#1c3353"
+				]
+			}, ]
 		},
 		options: {
 			responsive: true,
@@ -71,6 +63,9 @@
 						beginAtZero: true
 					}
 				}]
+			},
+			legend: {
+				display: false
 			}
 		}
 	});
