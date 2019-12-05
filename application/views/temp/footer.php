4b4abd3276e46f13d58c4427e9fa56fcfdd9aaa0
Elsa Maitsa <elsamaitsa@gmail.com>
2019-12-05 10:15:01 +0700

    El- edit menu

--- application/views/temp/footer.php
+++ application/views/temp/footer.php
@@ -1,99 +1,100 @@
 <!--footer-->
-					<footer class="footer">
-						<div class="container">
-							<div class="row align-items-center flex-row-reverse">
-								<div class="col-md-12 col-sm-12 text-center">
-									Copyright © 2019 <a href="#">Spaner</a>. Designed by <a href="https://spruko.com/">Spruko Technologies Pvt.Ltd</a> All rights reserved.
-								</div>
-							</div>
-						</div>
-					</footer>
-					<!-- End Footer-->
-				</div>
+<footer class="footer">
+	<div class="container">
+		<div class="row align-items-center flex-row-reverse">
+			<div class="col-md-12 col-sm-12 text-center">
+				Copyright © 2019 <a href="#">Spaner</a>. Designed by <a href="https://spruko.com/">Spruko Technologies
+					Pvt.Ltd</a> All rights reserved.
 			</div>
 		</div>
+	</div>
+</footer>
+<!-- End Footer-->
+</div>
+</div>
+</div>
 
-		<!-- Back to top -->
-		<a href="#top" id="back-to-top"><i class="fas fa-angle-up "></i></a>
+<!-- Back to top -->
+<a href="#top" id="back-to-top"><i class="fas fa-angle-up "></i></a>
 
-		<!--Jquery js -->
-		<script src="<?= base_url()?>assets/js/vendors/jquery-3.2.1.min.js"></script>
+<!--Jquery js -->
+<script src="<?= base_url()?>assets/js/vendors/jquery-3.2.1.min.js"></script>
 
-		<!--Jquery.Sparkline js -->
-		<script src="<?= base_url()?>assets/js/vendors/jquery.sparkline.min.js"></script>
+<!--Jquery.Sparkline js -->
+<script src="<?= base_url()?>assets/js/vendors/jquery.sparkline.min.js"></script>
 
-		<!--Circle-Progress js -->
-		<script src="<?= base_url()?>assets/js/vendors/circle-progress.min.js"></script>
+<!--Circle-Progress js -->
+<script src="<?= base_url()?>assets/js/vendors/circle-progress.min.js"></script>
 
-		<!--Jquery.rating js -->
-		<script src="<?= base_url()?>assets/plugins/jquery.rating/jquery.rating-stars.js"></script>
+<!--Jquery.rating js -->
+<script src="<?= base_url()?>assets/plugins/jquery.rating/jquery.rating-stars.js"></script>
 
-		<!--Bootstrap.min js-->
-		<script src="<?= base_url()?>assets/plugins/bootstrap/popper.min.js"></script>
-		<script src="<?= base_url()?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<!--Bootstrap.min js-->
+<script src="<?= base_url()?>assets/plugins/bootstrap/popper.min.js"></script>
+<script src="<?= base_url()?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
-		<!-- Custom scroll bar Js-->
-		<script src="<?= base_url()?>assets/plugins/jquery.mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-		
-			
-		<!-- Side menu js -->
-		<script src="<?= base_url()?>assets/plugins/sidemenu/js/sidemenu.js"></script>
-		<!-- Comment date : 12/2/2019 <script src="<?= base_url()?>assets/plugins/side-menu/js/side-menu.js"></script> -->
+<!-- Custom scroll bar Js-->
+<script src="<?= base_url()?>assets/plugins/jquery.mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
 
-		<!-- Sidemenu-responsive-tabs js-->
-		<script src="<?=base_url()?>assets/plugins/sidemenu-responsive-tabs/js/sidemenu-responsive-tabs.js"></script>
-		<script src="<?=base_url()?>assets/js/left-menu.js"></script>
-		
-		<!-- Inline js -->
-		<!-- Comment date : 12/2/2019 <script src="<?=base_url('assets/js/select2.js')?>"></script> -->
-		<!--Select2 js -->	
-		<script src="<?=base_url()?>assets/plugins/select2/select2.full.min.js"></script>
-		
-		<!-- P-scroll js -->
-		<script src="<?=base_url()?>assets/plugins/p-scroll/p-scroll.js"></script>
-		<script src="<?=base_url()?>assets/plugins/p-scroll/p-scroll-leftmenu.js"></script>
 
-		<!--Select2 js -->
-		<script src="<?=base_url()?>assets/plugins/select2/select2.full.min.js"></script>
+<!-- Side menu js -->
+<script src="<?= base_url()?>assets/plugins/sidemenu/js/sidemenu.js"></script>
+<!-- Comment date : 12/2/2019 <script src="<?= base_url()?>assets/plugins/side-menu/js/side-menu.js"></script> -->
 
-		<!-- peitychart -->
-		<script src="<?= base_url()?>assets/plugins/peitychart/jquery.peity.min.js"></script>
+<!-- Sidemenu-responsive-tabs js-->
+<script src="<?=base_url()?>assets/plugins/sidemenu-responsive-tabs/js/sidemenu-responsive-tabs.js"></script>
+<script src="<?=base_url()?>assets/js/left-menu.js"></script>
 
-		<!-- Timepicker js -->
-		<script src="<?=base_url()?>assets/plugins/jquery.timepicker/jquery.timepicker.js"></script>
-		<script src="<?base_url()?>assets/plugins/jquery.timepicker/toggles.min.js"></script>
+<!-- Inline js -->
+<!-- Comment date : 12/2/2019 <script src="<?=base_url('assets/js/select2.js')?>"></script> -->
+<!--Select2 js -->
+<script src="<?=base_url()?>assets/plugins/select2/select2.full.min.js"></script>
 
-		<!-- Datepicker js -->
-		<script src="<?=base_url()?>assets/plugins/date-picker/date-picker.js"></script>
-		<script src="<?=base_url()?>assets/plugins/date-picker/jquery-ui.js"></script>
-		<script src="<?=base_url()?>assets/plugins/jquery.mask/jquery.maskedinput.js"></script>
+<!-- P-scroll js -->
+<script src="<?=base_url()?>assets/plugins/p-scroll/p-scroll.js"></script>
+<script src="<?=base_url()?>assets/plugins/p-scroll/p-scroll-leftmenu.js"></script>
 
-		<!-- Inline js -->
-		<script src="<?=base_url()?>assets/js/select2.js"></script>
+<!--Select2 js -->
+<script src="<?=base_url()?>assets/plugins/select2/select2.full.min.js"></script>
 
-		<!-- Input Mask Plugin -->
-		<script src="<?= base_url()?>assets/plugins/jquery.mask/jquery.mask.min.js"></script>
+<!-- peitychart -->
+<script src="<?= base_url()?>assets/plugins/peitychart/jquery.peity.min.js"></script>
 
-		<!--Counters -->
-		<script src="<?= base_url()?>assets/plugins/counters/counterup.min.js"></script>
-		<script src="<?= base_url()?>assets/plugins/counters/waypoints.min.js"></script>
-		<!-- Chart js -->
-		<script src="<?=base_url()?>assets/plugins/chart/chart.bundle.js"></script>
-		<script src="<?=base_url()?>assets/js/chart.js"></script>
+<!-- Timepicker js -->
+<script src="<?=base_url()?>assets/plugins/jquery.timepicker/jquery.timepicker.js"></script>
+<script src="<?base_url()?>assets/plugins/jquery.timepicker/toggles.min.js"></script>
 
-		<!--Jquery.knob js-->
-		<script src="<?= base_url()?>assets/plugins/othercharts/jquery.knob.js"></script>
-		<script src="<?= base_url()?>assets/plugins/othercharts/othercharts.js"></script>
+<!-- Datepicker js -->
+<script src="<?=base_url()?>assets/plugins/date-picker/date-picker.js"></script>
+<script src="<?=base_url()?>assets/plugins/date-picker/jquery-ui.js"></script>
+<script src="<?=base_url()?>assets/plugins/jquery.mask/jquery.maskedinput.js"></script>
 
-		<!-- Sidebar js -->
-		<script src="<?= base_url()?>assets/plugins/sidebar/sidebar.js"></script>
+<!-- Inline js -->
+<script src="<?=base_url()?>assets/js/select2.js"></script>
 
-		<!--Index js -->
-		<script src="<?= base_url()?>assets/js/index2.js"></script>
-		<script src="<?= base_url()?>assets/js/index4.js"></script>
+<!-- Input Mask Plugin -->
+<script src="<?= base_url()?>assets/plugins/jquery.mask/jquery.mask.min.js"></script>
 
-		<!-- custom js -->
-		<script src="<?= base_url()?>assets/js/custom.js"></script>
-		<script src="<?= base_url()?>assets/public/js/app/api.js"></script>
+<!--Counters -->
+<script src="<?= base_url()?>assets/plugins/counters/counterup.min.js"></script>
+<script src="<?= base_url()?>assets/plugins/counters/waypoints.min.js"></script>
+<!-- Chart js -->
+<script src="<?=base_url()?>assets/plugins/chart/chart.bundle.js"></script>
+<script src="<?=base_url()?>assets/js/chart.js"></script>
 
-		<script src="https://rawgit.com/beaver71/Chart.PieceLabel.js/master/build/Chart.PieceLabel.min.js"></script>
+<!--Jquery.knob js-->
+<script src="<?= base_url()?>assets/plugins/othercharts/jquery.knob.js"></script>
+<script src="<?= base_url()?>assets/plugins/othercharts/othercharts.js"></script>
+
+<!-- Sidebar js -->
+<script src="<?= base_url()?>assets/plugins/sidebar/sidebar.js"></script>
+
+<!--Index js -->
+<script src="<?= base_url()?>assets/js/index2.js"></script>
+<script src="<?= base_url()?>assets/js/index4.js"></script>
+
+<!-- custom js -->
+<script src="<?= base_url()?>assets/js/custom.js"></script>
+<script src="<?= base_url()?>assets/public/js/app/api.js"></script>
+
+<script src="https://rawgit.com/beaver71/Chart.PieceLabel.js/master/build/Chart.PieceLabel.min.js"></script>
