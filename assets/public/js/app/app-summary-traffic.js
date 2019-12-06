0e0f53917cffb623b6a7bed1e80ff7399f555a2b
try haryatno <trybio123@gmail.com>
2019-12-06 13:15:14 +0700

    decimal di legend pie chart

--- assets/public/js/app/app-summary-traffic.js
+++ assets/public/js/app/app-summary-traffic.js
@@ -169,7 +169,8 @@ function drawChartAndCard(response){
                         total += parseInt(allData[i]);
                     }
                     // console.log(total)
-                    var percentage = Math.round((dataLabel / total) * 100);
+                    // var percentage = Math.round((dataLabel / total) * 100);
+                    var percentage = parseFloat((dataLabel / total)*100).toFixed(2);
                     legendHtml.push('<li class="col-md-4 col-lg-4 col-sm-6 col-xl-4">');
                     legendHtml.push('<span class="chart-legend"><div style="background-color:' + background + '" class="box-legend"></div>' + label + ' : ' + percentage + '%</span>');
                     legendHtml.push('</li>');
