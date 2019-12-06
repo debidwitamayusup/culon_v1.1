092629d4e6ea22ca61d611b2f12d340871604f31
Elsa Maitsa <elsamaitsa@gmail.com>
2019-12-06 16:37:20 +0700

    El-coba atur responsive

--- application/views/temp/header.php
+++ application/views/temp/header.php
@@ -4,6 +4,7 @@
 <head>
 	<!-- Meta data -->
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta content="Spaner - Simple light Bootstrap Nice Admin Panel Dashboard Design Responsive HTML5 Template"
