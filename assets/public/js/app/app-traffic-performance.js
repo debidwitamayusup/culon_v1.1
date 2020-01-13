0e188ab34e8343b9c38cbef3d4703aa96a24b199
dwittamma19@gmail.com <dwittamma19@gmail.com>
2020-01-13 16:05:25 +0700

    mengubah pembatas ribuan pada pie chart summary agent traffic performance #70

--- assets/public/js/app/app-traffic-performance.js
+++ assets/public/js/app/app-traffic-performance.js
@@ -110,6 +110,17 @@ function drawPie(response){
             //    }
                 display : false
             },
+            tooltips: {
+              callbacks: {
+                    label: function(tooltipItem, data) {
+                        var value = data.datasets[0].data[tooltipItem.index];
+                        value = value.toString();
+                        value = value.split(/(?=(?:...)*$)/);
+                        value = value.join(',');
+                        return value;
+                    }
+              } // end callbacks:
+            }, //end tooltips
             pieceLabel : {
                 render : 'legend',
                 fontColor : '#000',
