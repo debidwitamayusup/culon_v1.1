970133b74dbe7d897e7296281450c25ec28bba1e
Risyad Dimas <risyad.dimas@gmail.com>
2020-01-16 17:43:17 +0700

    Fixing conflict

--- assets/public/js/app/app-summary-ticket.js
+++ assets/public/js/app/app-summary-ticket.js
@@ -96,7 +96,6 @@ function simmiriStatusTicket(params, index, params_year){
     });
 }
 
-<<<<<<< HEAD
 function simmiriUnit(params, index, params_year){
     $("#filter-loader").fadeIn("slow");
     $.ajax({
