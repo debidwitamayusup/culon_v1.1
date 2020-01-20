ad35769eb6530250c05824fe4589f5d1153c5ec9
Risyad Dimas <risyad.dimas@gmail.com>
2020-01-20 17:48:46 +0700

    Done add commas on traffic interval

--- assets/public/js/app/app-traffic-today.js
+++ assets/public/js/app/app-traffic-today.js
@@ -33,7 +33,7 @@ function addCommas(commas)
     x2 = x.length > 1 ? '.' + x[1] : '';
     var rgx = /(\d+)(\d{3})/;
     while (rgx.test(x1)) {
-        x1 = x1.replace(rgx, '$1' + '.' + '$2');
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
     }
     return x1 + x2;
 }
@@ -273,7 +273,18 @@ function drawChartPercentageToday(response){
                 }],
                 xAxes: [{
                     ticks: {
-                        min: 0 // Edit the value according to what you need
+                        min: 0, // Edit the value according to what you need
+                        callback: function(value, index, values) {
+                           //      if(parseInt(value) >= 1000){
+                           //          var res = (value/1000);
+                                    // return res+'K'
+                           //      } else
+                           //       return value;
+                            value = value.toString();
+                            value = value.split(/(?=(?:...)*$)/);
+                            value = value.join(',');
+                            return value;
+                        }
                     }
                 }]
             },
