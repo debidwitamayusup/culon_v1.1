193c279407988957b6de907bcba60fbc3bbebe57
Risyad Dimas <risyad.dimas@gmail.com>
2020-01-20 11:07:42 +0700

    Recommit 'Fixing add commas tooltip perofrmance by channel'

--- assets/public/js/app/app-performance-channel.js
+++ assets/public/js/app/app-performance-channel.js
@@ -205,9 +205,9 @@ function drawChartSumService(response){
 	                        beginAtZero:true,
 	                        callback: function(value, index, values) {
 	                            if(parseInt(value) >= 1000){
-	                               return '' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	                               return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	                            } else {
-	                               return '' + value;
+	                               return value;
 	                            }
 	                       }                            
 	                    }
