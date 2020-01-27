75252b0770085172f7bce97e5353bd7bc888f02e
elsamaitsa <elsamaitsa@gmail.com>
2020-01-27 18:31:56 +0700

    el-edit add page

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -33,11 +33,17 @@ class main extends CI_Controller {
 
 	public function home()
 	{
-
 		$this->load->view('temp/header');
 		$this->load->view('temp/navbar');
 		$this->load->view('temp/sidebar');
         $this->load->view('temp/body');
+	}
+
+	public function summary_traffic(){
+		$this->load->view('temp/header');
+		$this->load->view('temp/navbar');
+		$this->load->view('temp/sidebar');
+		$this->load->view('v_summary_traffic');
 	}
 
 	public function login(){
@@ -307,13 +313,4 @@ class main extends CI_Controller {
 		$this->load->view('temp/sidebar');
 		$this->load->view('v_report_summary_interval');
 	}
-
-	public function cek(){
-		$this->load->view('temp/header');
-		$this->load->view('temp/navbar');
-		$this->load->view('temp/sidebar');
-		$this->load->view('cek.php');
-		$this->load->view('temp/footer');
-	}
-
 }
