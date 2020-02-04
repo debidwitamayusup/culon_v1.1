0b168e8618cb8827f055c6d80e23f84cf62732cb
dwittamma19@gmail.com <dwittamma19@gmail.com>
2020-02-04 17:18:44 +0700

    proses export performance operation

--- application/views/v_report_performance_operation.php
+++ application/views/v_report_performance_operation.php
@@ -203,24 +203,3 @@
             <?php $this->load->view('temp/footer');?>
             <!--Plugin -->
             <script src="<?=base_url()?>assets/public/js/app/app-report-operation.js"></script>
-             <!--Required scripts-->
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <!-- External files for exporting -->
-    <script src="https://www.igniteui.com/js/external/FileSaver.js"></script>
-    <script src="https://www.igniteui.com/js/external/Blob.js"></script>
-
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/infragistics.core.js"></script>
-
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_core.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_collections.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_text.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_io.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_ui.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.documents.core_core.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_collectionsextended.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.excel_core.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_threading.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.ext_web.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.xml.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.documents.core_openxml.js"></script>
-    <script type="text/javascript" src="http://cdn-na.infragistics.com/igniteui/2019.2/latest/js/modules/infragistics.excel_serialization_openxml.js"></script>
