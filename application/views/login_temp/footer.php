48b6a19a4079bb4853442e5129cf5e6a251ae268
elsamaitsa <elsamaitsa@gmail.com>
2020-02-14 11:59:08 +0700

    el-edit path

--- application/views/login_temp/footer.php
+++ application/views/login_temp/footer.php
@@ -5,14 +5,14 @@
 		<script src="<?php echo base_url();?>assets/js/vendors/selectize.min.js"></script>
 		<script src="<?php echo base_url();?>assets/js/vendors/jquery.tablesorter.min.js"></script>
 		<script src="<?php echo base_url();?>assets/js/vendors/circle-progress.min.js"></script>
-		<script src="<?php echo base_url();?>assets/js/vendors/jquery.rating/jquery.rating-stars.js"></script>
+		<script src="<?php echo base_url();?>assets/js/vendors/jquery.rating-stars.js"></script>
 
 		<!--Bootstrap.min js-->
 		<script src="<?php echo base_url();?>assets/js/vendors/popper.min.js"></script>
 		<script src="<?php echo base_url();?>assets/js/bootstrap/bootstrap.min.js"></script>
 
 		<!-- Custom scroll bar js-->
-		<script src="<?php echo base_url();?>assets/js/vendors/jquery.mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+		<script src="<?php echo base_url();?>assets/js/vendors/jquery.mCustomScrollbar.concat.min.js"></script>
 
 		<!--Peitychart js-->
 		<script src="<?php echo base_url();?>assets/js/peitychart/jquery.peity.min.js"></script>
