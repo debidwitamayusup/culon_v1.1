bea4c1123a30ef04280a303062318a4b1e977e87
dwittamma19@gmail.com <dwittamma19@gmail.com>
2020-02-20 15:45:18 +0700

    draw table looping report sum interval

--- application/views/v_report_summary_traffic.php
+++ application/views/v_report_summary_traffic.php
@@ -67,8 +67,10 @@
                                 <tr>
                                     <td rowspan="2" class="wd-15p border-bottom-0 align-middle" width="20">No</td>
                                     <td rowspan="2" class="wd-15p border-bottom-0 align-middle">Date</td>
+                                    <td colspan="2" class="wd-15p border-bottom-0">Voice</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Email</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Live Chat</td>
+                                    <td colspan="2" class="wd-15p border-bottom-0">SMS</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Telegram</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Facebook</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Messenger</td>
@@ -78,8 +80,6 @@
                                     <td colspan="2" class="wd-15p border-bottom-0">Whatsapp</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Twitter DM</td>
                                     <td colspan="2" class="wd-15p border-bottom-0">Chatbot</td>
-                                    <td colspan="2" class="wd-15p border-bottom-0">Voice</td>
-                                    <td colspan="2" class="wd-15p border-bottom-0">SMS</td>
                                 </tr>
                                 <tr>
                                 <td>COF</td>
