c5701d1a19699496bc2a211ee9cc1f9810d975dd
elsamaitsa <elsamaitsa@gmail.com>
2020-02-26 16:03:51 +0700

    el-create

---
+++ application/views/503.php
@@ -0,0 +1,12 @@
+	<body>
+
+		<!-- Global Loader-->
+		<div id="global-loader"><img src="<?=base_url()?>assets/images/svgs/loader.svg" alt="loader"></div>
+
+		<div class="page">
+			<div class="container text-center">
+				<div class="display-1  mb-5">503</div>
+				<h1 class="h2  mb-3">Server Not Found...</h1>
+					<p class="h4 font-weight-normal mb-7 leading-normal ">oops! Something goes Wrong.</p>
+			</div>
+		</div>
