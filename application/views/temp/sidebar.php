0a2734382c54b40f08070853a3c2b95ae286590d
elsamaitsa <elsamaitsa@gmail.com>
2020-02-27 14:32:53 +0700

    el-edit

--- application/views/temp/sidebar.php
+++ application/views/temp/sidebar.php
@@ -38,7 +38,7 @@
 							<a class="slide-item" href="<?=base_url()?>main/wall_performance_operation"> Summary
 								Performance Operation</a>
 							<a class="slide-item" href="<?=base_url()?>main/wall_summary_performance"> Summary
-								Performance</a>
+								Performance Realtime</a>
 							<div class="side-menu p-0">
 								<div class="slide submenu">
 									<a class="side-menu__item" data-toggle="slide" href="#"><span
