76f4cbf2ca41055b459a64613c19ae7f416fc7d2
Risyad Dimas <risyad.dimas@gmail.com>
2020-03-03 16:58:03 +0700

    Fixing problem in wall summ perform realtime

--- application/views/v_wall_summary_performance.php
+++ application/views/v_wall_summary_performance.php
@@ -1,75 +1,13 @@
 <div class=" app-content">
     <div class="side-app">
         <div class="page-header d-flex bd-highlight">
-            <div class="mr-auto bd-highlight">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item active" aria-current="page">
-                        <h4 class="page-title"><i class="fe fe-grid mr-1"></i>Wallboard</h4>
-                    </li>
-                    <li class="breadcrumb-item active mt-2" aria-current="page">Summary Performance Realtime</li>
-                </ol>
-            </div>
-            <div class="bd-highlight">
-                <div class="bd-highlight" style="margin-left:5px;">
-                    <div class="card-options d-none d-sm-block">
-                        <div class="btn-group text-center btn-sm">
-                            <a href="#" class="btn btn-light btn-sm" id="btn-day">
-                                <span class="">Day</a></span>
-                            <a href="#" class="btn btn-light btn-sm" id="btn-month">
-                                <span class="">Month</a></span>
-                            <a href="#" class="btn btn-light btn-sm" id="btn-year">
-                                <span class="">Year</a></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="bd-highlight">
-                    <!-- daily -->
-                    <div id="filter-date" class="mt-1 mr-0">
-                        <input id="input-date-filter" class="w-55 ml-auto form-control form-control-sm fc-datepicker"
-                            placeholder="MM/DD/YYYY" type="text">
-                    </div>
-
-                    <!-- monthly -->
-                    <div id="filter-month" class="row mt-1 mr-0">
-                        <div class="col-md-auto">
-                            <select name="select-month" id="select-month" class="form-control form-control-sm">
-                                <option value="1">January</option>
-                                <option value="2">February</option>
-                                <option value="3">March</option>
-                                <option value="4">April</option>
-                                <option value="5">May</option>
-                                <option value="6">June</option>
-                                <option value="7">July</option>
-                                <option value="8">August</option>
-                                <option value="9">September</option>
-                                <option value="10">October</option>
-                                <option value="11">November</option>
-                                <option value="12">December</option>
-                            </select>
-                        </div>
-                        <!-- Monthly -->
-                        <div>
-                            <select name="select-year-on-month" id="select-year-on-month"
-                                class="form-control form-control-sm">
-                            </select>
-                        </div>
-                        <!-- Monthly -->
-                        <div>
-                            <span class="col-auto">
-                                <button class="btn btn-sm btn-dark" type="button" style="height:29px" id="btn-go"><i
-                                        class="fe fe-arrow-right text-white"></i></button>
-                            </span>
-                        </div>
-                    </div>
-
-                    <!-- yearly -->
-                    <div id="filter-year" class="mt-1 mr-0">
-                        <select name="select-year-only" id="select-year-only" class="form-control form-control-sm">
-                        </select>
-                    </div>
-                    <!-- yearly -->
-                </div>
-            </div>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page">
+                    <h4 class="page-title"><i class="fe fe-monitor mr-1"></i>Wallboard</h4>
+                </li>
+                <li class="breadcrumb-item active mt-2" aria-current="page">Summary Performance Realtime
+                </li>
+            </ol>
         </div>
         <!-- <div class="d-flex bd-highlight">
             <div class="ml-auto p-2 bd-highlight mt-3 h6">Layanan </div>
