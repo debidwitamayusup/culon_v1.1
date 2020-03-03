c39176564a0183f6cd3f457e7c4c0debec899dcb
Risyad Dimas <risyad.dimas@gmail.com>
2020-03-03 16:46:38 +0700

    Add function filter time in menu dash summ perform realtime and wall summ perform realtime

--- assets/public/js/app/app-summary-performance-realtime.js
+++ assets/public/js/app/app-summary-performance-realtime.js
@@ -24,13 +24,13 @@ if (n < 10) {
 var v_params_this_year = m + '-' + n + '-' + (o);
 var arr_tenant = [];
 const sessionParams = JSON.parse(sessionStorage.getItem('Auth-infomedia'));
-// if(sessionParams.TENANT_ID[0].TENANT_ID != ''){
-//     for(var i=0; i < sessionParams.TENANT_ID.length; i++){
-//         arr_tenant.push(sessionParams.TENANT_ID[i].TENANT_ID);
-//     }
-// }else{
-//     arr_tenant = [];
-// }
+if(sessionParams.TENANT_ID[0].TENANT_ID != ''){
+    for(var i=0; i < sessionParams.TENANT_ID.length; i++){
+        arr_tenant.push(sessionParams.TENANT_ID[i].TENANT_ID);
+    }
+}else{
+    arr_tenant = [];
+}
 $(document).ready(function(){
     if(sessionParams){
         $('#select-month option[value='+n+']').attr('selected','selected');
@@ -45,11 +45,11 @@ $(document).ready(function(){
         sessionStorage.removeItem('paramsSession');
         sessionStorage.setItem('paramsSession', 'day');
 
-        // if(sessionParams.TENANT_ID[0].TENANT_ID != ''){
-        //     getTenant('', sessionParams.USERID);
-        // }else{
-        //     getTenant('', '');
-        // }
+        if(sessionParams.TENANT_ID[0].TENANT_ID != ''){
+            getTenant('', sessionParams.USERID);
+        }else{
+            getTenant('', '');
+        }
 
         $("#filter-loader").fadeIn("slow");
         callThreeTable('', arr_tenant);
@@ -179,34 +179,34 @@ function monthNumToName(month) {
     return months[month - 1] || '';
 }
 
-// function getTenant(date, userid){
-//     $.ajax({
-//         type: 'POST',
-//         url: base_url + 'api/Wallboard/WallboardController/GetTennantFilter',
-//         data: {
-//             "date" : date,
-//             "userid" : userid
-//         },
+function getTenant(date, userid){
+    $.ajax({
+        type: 'POST',
+        url: base_url + 'api/Wallboard/WallboardController/GetTennantFilter',
+        data: {
+            "date" : date,
+            "userid" : userid
+        },
 
-//         success: function (r) {
-//             var data_option = [];
-//             //dont parse response if using rest controller
-//             // var response = JSON.parse(r);
-//             var response = r;
-//             // console.log(response);
-//             // tenants = response.data;
-//                 var html = '';
-//                 for(i=0; i<response.data.length; i++){
-//                     html += '<option value='+response.data[i].TENANT_ID+'>'+response.data[i].TENANT_NAME+'</option>';
-//                 }
-//                 $('#layanan_name').html(html);
-//         },
-//         error: function (r) {
-//             //console.log(r);
-//             alert("error");
-//         },
-//     });
-// }
+        success: function (r) {
+            var data_option = [];
+            //dont parse response if using rest controller
+            // var response = JSON.parse(r);
+            var response = r;
+            // console.log(response);
+            // tenants = response.data;
+                var html = '';
+                for(i=0; i<response.data.length; i++){
+                    html += '<option value='+response.data[i].TENANT_ID+'>'+response.data[i].TENANT_NAME+'</option>';
+                }
+                $('#layanan_name').html(html);
+        },
+        error: function (r) {
+            //console.log(r);
+            alert("error");
+        },
+    });
+}
 
 function callThreeTable(date, tenant_id){
     $.ajax({
