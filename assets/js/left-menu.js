6e921694bdfaef5efff264e284a5dcab94b448cb
elsamaitsa <elsamaitsa@gmail.com>
2020-03-13 10:57:40 +0700

    el-edit

--- assets/js/left-menu.js
+++ assets/js/left-menu.js
@@ -3,7 +3,7 @@
 	
 	//Active Class
 	$(".app-sidebar #sidemenu-Tab a").each(function() {
-		var pageUrl = window.location.href.split(/[?#]/)[0];
+		var pageUrl = window.location.href;
 		if (this.href == pageUrl) {
 			$(this).addClass("active");
 			$(this).parent().parent().parent().addClass("active"); // add active to li of the current link
