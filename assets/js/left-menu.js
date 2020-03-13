3ad73829859aa3c7271fb246d69ca97b1dd627d9
elsamaitsa <elsamaitsa@gmail.com>
2020-03-13 11:28:00 +0700

    el-edit remove hash per tab menu

--- assets/js/left-menu.js
+++ assets/js/left-menu.js
@@ -3,7 +3,7 @@
 	
 	//Active Class
 	$(".app-sidebar #sidemenu-Tab a").each(function() {
-		var pageUrl = window.location.href;
+		var pageUrl = window.location.href.split(/[?#]/)[0];
 		if (this.href == pageUrl) {
 			$(this).addClass("active");
 			$(this).parent().parent().parent().addClass("active"); // add active to li of the current link
