0a7c7cd9dd10ef7e6a0cef05443ced46f78b3045
dwittamma19 <dwittamma19@gmail.com>
2020-03-17 11:44:57 +0700

    user admin

--- assets/public/js/app/app-admin-user.js
+++ assets/public/js/app/app-admin-user.js
@@ -1,47 +1,65 @@
 var base_url = $('#base_url').val();
-const sessionParams = JSON.parse(localStorage.getItem('Auth-infomedia'));
+const sessionParams = JSON.parse(localStorage.getItem('Auth-token'));
 $(document).ready(function(){
     if(sessionParams){
-        callTableUser();
+        callTableUser(sessionParams);
     }else{
         window.location = base_url
     }
 });
 
-function callTableUser(){
+function callTableUser(token){
     $("#filter-loader").fadeIn("slow");
-	$('#tableAgent').DataTable({
+	var thisTable = $('#tableAdmin_user').DataTable({
         ajax: {
-            url : base_url + 'api/Reporting/ReportController/ReportingAL',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("token", token);
+            },
+            url : base_url + 'api/Auth/AuthController/usrlst',
             type : 'POST',
-            data :{
-                tenant_id: tenant_id,
-                start_date: start_date,
-                end_date: end_date
-            }
         },
         columnDefs: [
 			{ className: "text-center", targets: 0 },
 			{ className: "text-center", targets: 1 },
-			{ className: "text-center", targets: 2 },
-			{ className: "text-left", targets: 3 },
-			{ className: "text-center", targets: 4 },
-			{ className: "text-center", targets: 5 },
-			{ className: "text-center", targets: 6 },
-			{ className: "text-center", targets: 7 },
-			{ className: "text-center", targets: 8 },
-			{ className: "text-center", targets: 9 },
-			{ className: "text-center", targets: 10 },
-			{ className: "text-center", targets: 11 },
+			{ className: "text-left", targets: 2 },
+			{ className: "text-center", targets: 3 },
+            { className: "text-center", targets: 4 },
+            { className: "text-center", targets: 5 },
+            {
+                className: "text-center",
+                targets: -1,
+                // defaultContent: ['<a href="'+base_url+'admin/edit_user" class="btn btn-sm btn-grey-1 style-btn mr-2 editButton">Edit User</a><a class="btn btn-sm btn-blue style-btn">Reset Password</a>']
+                defaultContent: ['<a href="#" class="btn btn-sm btn-grey-1 style-btn mr-2 editButton">Edit User</a><a class="btn btn-sm btn-blue style-btn">Reset Password</a>']
+                
+            }
+			
 		], 
         destroy: true,
         fnInfoCallback: function( oSettings, iStart, iEnd, iMax, iTotal, sPre ) {
             return "Showing "+addCommas(iStart) +" to "+ addCommas(iEnd) +" from " + addCommas(iTotal)+" entries";
         },
     });
+    $('body').on('click', '#tableAdmin_user tbody tr .editButton', function () {
+        var rowData = thisTable.row( $(this).parents('tr')).data();
+        // console.log("Rows data : ",  rowData);
+        sessionStorage.setItem('editUser',rowData);
+});
     $("#filter-loader").fadeOut("slow");
 }
 
-(function(){
-    $('#tableAdmin_user').dataTable();
-})(jQuery);
+function addCommas(commas)
+{
+    commas += '';
+    x = commas.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + '.' + '$2');
+    }
+    return x1 + x2;
+}
+
+// (function(){
+//     $('#tableAdmin_user').dataTable();
+// })(jQuery);
