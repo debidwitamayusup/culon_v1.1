c36cf4f133b22e6d8098ff5e1931e42485ea9696
dwittamma19 <dwittamma19@gmail.com>
2020-03-20 18:17:29 +0700

    tenant list

--- assets/public/js/app/app-admin-tenant.js
+++ assets/public/js/app/app-admin-tenant.js
@@ -1,3 +1,40 @@
-(function(){
-    $('#tableListTenant').dataTable();
-})(jQuery);
+var base_url = $('#base_url').val();
+const sessionParams = JSON.parse(localStorage.getItem('Auth-token'));
+const userParams = JSON.parse(localStorage.getItem('Auth-infomedia'));
+$(document).ready(function(){
+    if(sessionParams){
+        if(userParams.PREVILAGE != "admin"){
+            window.location = base_url
+        }else{
+            callTableTenant(sessionParams);
+        }
+    }else{
+        window.location = base_url
+    }
+});
+
+function callTableTenant(token){
+    $("#filter-loader").fadeIn("slow");
+	var thisTable = $('#tableListTenant').DataTable({
+        ajax: {
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("token", token);
+            },
+            url : base_url + 'api/Superadmin/SuperadminController/tntlst',
+            type : 'POST',
+        },
+        columnDefs: [
+			{ className: "text-center", targets: 0 },
+			{ className: "text-center", targets: 1 },
+			{ className: "text-left", targets: 2 }
+			
+		], 
+        destroy: true
+    });
+    $("#filter-loader").fadeOut("slow");
+}
+
+
+// (function(){
+//     $('#tableListTenant').dataTable();
+// })(jQuery);
