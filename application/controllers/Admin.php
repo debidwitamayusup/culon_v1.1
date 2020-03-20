56169960e5366395608818409a0305b9a5dba73c
elsamaitsa <elsamaitsa@gmail.com>
2020-03-20 17:09:06 +0700

    el-create new tampilan

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -38,4 +38,10 @@ class admin extends CI_Controller {
 		$this->load->view('v_list_tenant');
 	}
 
+	public function add_tenant(){
+		$this->load->view('temp/header');
+		$this->load->view('temp/navbar');
+		$this->load->view('temp/sidebar_admin');
+		$this->load->view('v_add_tenant');
+	}
 }
