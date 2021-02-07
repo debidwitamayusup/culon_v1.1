5a4464ee63229f2e53a1ed0088d8d8232938c4eb
dwittamma19@gmail.com <dwittamma19@gmail.com>
2021-02-07 19:55:13 +0700

    lama gabung

--- assets/public/js/app/detil-pengajuan-cuti.js
+++ assets/public/js/app/detil-pengajuan-cuti.js
@@ -83,7 +83,8 @@ function getDataKaryawan(username) {
             $('#id_karyawan').val(response.data.nomorInduk);
             $('#nama_karyawan').val(response.data.nama);
             $('#tgl_gabung').val(response.data.tglGabung);
-            $('#lama_gabung').val(durasi_hari + " hari");
+            // $('#lama_gabung').val(durasi_hari + " hari");
+            $('#lama_gabung').val(getAge(new Date(response.data[0].dataKaryawan.tglGabung), new Date()));
             $('#id_leader').val(response.data.dataLeader.nomorIndukLeader);
             $('#nama_leader').val(response.data.dataLeader.namaLeader);
 
@@ -130,7 +131,8 @@ function getDetilCuti(idUnikCuti) {
             let date2 = new Date(v_params_this_year)
             let beda_tgl = date2.getTime() - date1.getTime()
             let durasi_hari = beda_tgl / (1000 * 3600 * 24)
-            $('#lama_gabung').val(durasi_hari + " hari");
+            // $('#lama_gabung').val(durasi_hari + " hari");
+            $('#lama_gabung').val(getAge(new Date(response.data[0].dataKaryawan.tglGabung), new Date()));
             $('#id_leader').val(response.data[0].dataKaryawan.dataLeader.nomorIndukLeader);
             $('#nama_leader').val(response.data[0].dataKaryawan.dataLeader.namaLeader);
 
@@ -351,6 +353,49 @@ function cetakPengajuan(id_karyawan, namaCuti, durasiCuti, startDate, endDate, a
     });
     */
 }
+
+function getAge(date_1, date_2)
+    {
+    
+        //convert to UTC
+        var date2_UTC = new Date(Date.UTC(date_2.getUTCFullYear(), date_2.getUTCMonth(), date_2.getUTCDate()));
+        var date1_UTC = new Date(Date.UTC(date_1.getUTCFullYear(), date_1.getUTCMonth(), date_1.getUTCDate()));
+
+
+        var yAppendix, mAppendix, dAppendix;
+
+
+        //--------------------------------------------------------------
+        var days = date2_UTC.getDate() - date1_UTC.getDate();
+        if (days < 0)
+        {
+
+            date2_UTC.setMonth(date2_UTC.getMonth() - 1);
+            days += DaysInMonth(date2_UTC);
+        }
+        //--------------------------------------------------------------
+        var months = date2_UTC.getMonth() - date1_UTC.getMonth();
+        if (months < 0)
+        {
+            date2_UTC.setFullYear(date2_UTC.getFullYear() - 1);
+            months += 12;
+        }
+        //--------------------------------------------------------------
+        var years = date2_UTC.getFullYear() - date1_UTC.getFullYear();
+
+
+
+
+        if (years > 1) yAppendix = " tahun";
+        else yAppendix = " tahun";
+        if (months > 1) mAppendix = " bulan";
+        else mAppendix = " bulan";
+        if (days > 1) dAppendix = " hari";
+        else dAppendix = " hari";
+
+
+        return years + yAppendix + ", " + months + mAppendix + ", " + days + dAppendix;
+    }
 
 //jquery
 (function($) {
